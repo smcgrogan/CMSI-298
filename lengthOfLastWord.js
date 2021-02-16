5e88cd8cba77fb4879f1154f7e11df03e491aeff
smcgrogan <58006153+smcgrogan@users.noreply.github.com>
2021-02-16 11:16:33 -0800

    Corrected lengthoflastword
    Corrected errors.

---
+++ lengthOfLastWord.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let length = 0;
+    for (let i = s.length - 1; i >= 0; i--){
+        if(s[i] == ' ' && length !== 0){
+            return length;
+        }
+        else{
+            length++;
+        }
+    }
+    return length;
+    
+};
